e156bd9ffe8bee64899a00032c5b442311d276d6
Fergus Molloy <fergus@molloy.xyz>
2022-10-14 14:51:14 +0100

    added wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+    font = wezterm.font("Fira Code", {weight="Regular", stretch="Normal", style="Normal"}),
+    font_size = 14.0,
+    term = "wezterm",
+    enable_tab_bar = false,
+    color_scheme = "Gruvbox dark, medium (base16)"
+}
